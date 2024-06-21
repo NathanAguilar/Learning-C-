b84750b598861f3b0eacc883bd7f1375f82b088c
NathanAguilar <nathan.rene.aguilar@gmail.com>
2024-06-21 14:32:43 -0700

    Overview of CSharp

---
+++ Overview/Comments/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML Comments are used to help provide documentation
+        /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// this is the main sample application function
+        /// </summary>
+        /// <param name='args'>An array of string arguments from the command line</param>
+        /// <returns>
+        /// No return Value
+        /// </returns>
+        static void Main(string[] args)
+        {
+            // Single line comments start with two slashes
+            Console.WriteLine("Hello World!");
+            /* Multiple line comments start with a slash and a star 
+            and can continue for several lines
+            until a closing star and slash are encountered */
+        }
+    }
+}
---
+++ Overview/HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Console.WriteLine("Hello World!");
+Console.WriteLine("What is your name?");
+string str = Console.ReadLine();
+Console.WriteLine("Why, hello there " + str);
+ 
